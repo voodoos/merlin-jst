7099aa67a70b6c2c6764d73e1c71a75eab858753
Ulysse Gérard <thevoodoos@gmail.com>
2025-09-19 10:55:58 +0200

    Fix the set ordering

--- src/ocaml/typing/discourse_types.ml
+++ src/ocaml/typing/discourse_types.ml
@@ -3,13 +3,8 @@ module Paths = struct
   module T = struct
     type t = Shape.Sig_component_kind.t * Path.t
 
-    let rec length acc = function
-      | Path.Pident _ -> acc + 1
-      | Pdot (p, _) | Papply (p, _) | Pextra_ty (p, _) -> length (acc + 1) p
-
-    let length (_, p) = length 0 p
-
-    let compare p1 p2 = if p1 == p2 then 0 else length p1 - length p2
+    let compare (_, p1) (_, p2) =
+      Path.compare p1 p2
   end
 
   include Set.Make (T)
