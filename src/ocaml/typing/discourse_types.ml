3889ee98c41fa27fd7ed67ccf9c5684cb3fa68cf
Ulysse Gérard <thevoodoos@gmail.com>
2025-11-20 11:56:21 +0100

    Remove outdated comment

--- src/ocaml/typing/discourse_types.ml
+++ src/ocaml/typing/discourse_types.ml
@@ -165,6 +165,7 @@ module Lid_trie = struct
   (* let _ =
      let t =
        empty
+       |> add (Lident "F") (Value, Pident (Ident.create_persistent "F"))
        |> add
             (Ldot (Lapply (Lident "F", Lident "A"), "x"))
             (Value, Pident (Ident.create_persistent "totoid"))
@@ -172,13 +173,20 @@ module Lid_trie = struct
             (Ldot (Ldot (Lident "F", "B"), "x"))
             (Type, Pident (Ident.create_persistent "totoid2"))
      in
+     let _pp_lid_paths ppf (lid, paths) =
+       Format.fprintf ppf "%a -> %a" Pprintast.longident lid pp_paths paths
+     in
      Format.eprintf "\nTRIE %a\n%!" pp t;
-     Format.eprintf "TRIESEQ %a\n%!"
-       (Format.pp_print_seq pp_lid_paths)
-       (to_seq t);
-     Format.eprintf "TRIEACHED %a\n%!"
-       (Format.pp_print_seq pp_lid_paths)
-       (to_seq (reach t (Lapply (Lident "F", Lident "A")) |> Option.get)) *)
+     let _seq = to_seq t in
+     ignore (Format.eprintf "%a\n" (Format.pp_print_seq _pp_lid_paths) _seq);
+     Format.eprintf "\nEND TEST PRINT\n%!" *)
+
+  (* Format.eprintf "TRIESEQ %a\n%!"
+     (Format.pp_print_seq _pp_lid_paths)
+     (to_seq t); *)
+  (* Format.eprintf "TRIEACHED %a\n%!"
+     (Format.pp_print_seq _pp_lid_paths)
+     (to_seq (reach t (Lapply (Lident "F", Lident "A")) |> Option.get)) *)
 end
 
 type t = Lid_trie.t
