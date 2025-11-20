1446354c70af0e8e956e91ee1707db1cdcfabeda
Ulysse Gérard <thevoodoos@gmail.com>
2025-11-20 12:07:02 +0100

    Remove unused custom path comparison

--- src/ocaml/typing/discourse_types.ml
+++ src/ocaml/typing/discourse_types.ml
@@ -1,39 +1,5 @@
 (* This module exists to prevent a dependency cycle with Types. *)
 
-let compare_paths ?(compare_idents = Ident.compare)
-    ?(compare_strings = String.compare) (p1 : Path.t) (p2 : Path.t) =
-  let rec compare_paths (p1 : Path.t) (p2 : Path.t) =
-    if p1 == p2 then 0
-    else
-      match (p1, p2) with
-      | Path.Pident id1, Pident id2 -> compare_idents id1 id2
-      | Pdot (p1, s1), Pdot (p2, s2) ->
-        let h = compare_paths p1 p2 in
-        if h <> 0 then h else compare_strings s1 s2
-      | Papply (fun1, arg1), Papply (fun2, arg2) ->
-        let h = compare_paths fun1 fun2 in
-        if h <> 0 then h else compare_paths arg1 arg2
-      | Pextra_ty (p1, t1), Pextra_ty (p2, t2) ->
-        let h = compare_extra t1 t2 in
-        if h <> 0 then h else compare_paths p1 p2
-      | Pident _, (Pdot _ | Papply _ | Pextra_ty _)
-      | Pdot _, (Papply _ | Pextra_ty _)
-      | Papply _, Pextra_ty _ -> -1
-      | (Pextra_ty _ | Papply _ | Pdot _), Pident _
-      | (Pextra_ty _ | Papply _), Pdot _
-      | Pextra_ty _, Papply _ -> 1
-  and compare_extra (t1 : Path.extra_ty) (t2 : Path.extra_ty) =
-    match (t1, t2) with
-    | Pcstr_ty s1, Pcstr_ty s2 -> compare_strings s1 s2
-    | Pext_ty, Pext_ty -> 0
-    | Punboxed_ty, Punboxed_ty -> 0
-    | Pcstr_ty _, (Pext_ty | Punboxed_ty) -> -1
-    | (Pext_ty | Punboxed_ty), Pcstr_ty _ -> 1
-    | Pext_ty, Punboxed_ty -> -1
-    | Punboxed_ty, Pext_ty -> 1
-  in
-  compare_paths p1 p2
-
 let rec compare_longidents ?(compare_strings = String.compare)
     (l1 : Longident.t) (l2 : Longident.t) =
   match (l1, l2) with
@@ -57,7 +23,7 @@ module Paths = struct
         priority queue) before shortening, it does not seems useful tu use a
         custom path comparison function here. *)
 
-    let compare (_, p1) (_, p2) = compare_paths p1 p2
+    let compare (_, p1) (_, p2) = Path.compare p1 p2
   end
 
   include Set.Make (T)
